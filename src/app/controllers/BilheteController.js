f48372c88679dbf47ee9396f6341636a3b03fe50
humiGod <71161198+humiGod@users.noreply.github.com>
2021-02-12 13:44:19 -0300

    adicionar bilhete

--- src/app/controllers/BilheteController.js
+++ src/app/controllers/BilheteController.js
@@ -1,4 +1,5 @@
 import Bilhete from '../models/Bilhete';
+import * as Yup from 'yup';
 
 class BilheteController{
     async index(req, res){
@@ -6,7 +7,18 @@ class BilheteController{
     }
 
     async store(req, res){
+        const schema = Yup.object().shape({
+            id_sessao: Yup.number().required().positive().integer(),
+            id_sala: Yup.number().required().positive().integer(),
+            id_poltrona: Yup.number().required().positive().integer()
+        });
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({error: "Falha na Transmição de Dados"})
+        }
+        const data = {id_sessao: req.body.id_sessao, id_sala: req.body.id_sala, id_poltrona: req.body.id_poltrona, reservado: false}
+        await Bilhete.create(data);
 
+        return res.status(200).json({sucesso: "Bilhete Criado"})
     }
 
     async update(req, res){
--- src/app/models/Bilhete.js
+++ src/app/models/Bilhete.js
@@ -3,8 +3,9 @@ import Sequelize, {Model} from 'sequelize';
 class Bilhete extends Model{
     static init(sequelize){
         super.init({
-            id_poltrona: Sequelize.INTEGER,
             id_sessao: Sequelize.INTEGER,
+            id_sala: Sequelize.INTEGER,
+            id_poltrona: Sequelize.INTEGER,
             reservado: Sequelize.BOOLEAN
         },{
             sequelize
