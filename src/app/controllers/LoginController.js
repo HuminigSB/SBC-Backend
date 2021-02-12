2270071937526688d1984c3b0375d3970e27a8f7
MatheusINFO <matheus.add12@gmail.com>
2021-02-12 12:05:57 -0300

    Removido bug de start

--- src/app/controllers/LoginController.js
+++ src/app/controllers/LoginController.js
@@ -1,7 +1,21 @@
 import User from '../models/User';
 
 class LoginController{
+    async index(req, res){
+  
+    }
 
+    async store(req, res){
+
+    }
+
+    async update(req, res){
+
+    }
+
+    async delete(req, res){
+        
+    }
 }
 
 export default new LoginController();
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,21 @@
 import User from '../models/User';
 
 class UserController{
+    async index(req, res){
+  
+    }
+
+    async store(req, res){
+
+    }
+
+    async update(req, res){
+
+    }
+
+    async delete(req, res){
+        
+    }
 }
 
 export default new UserController();
