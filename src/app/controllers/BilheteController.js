304a7391abbf6184c22399155977fa25e89fbb60
humiGod <71161198+humiGod@users.noreply.github.com>
2021-02-17 18:01:51 -0300

    clean code adicionar bilhete

--- src/app/controllers/BilheteController.js
+++ src/app/controllers/BilheteController.js
@@ -15,7 +15,8 @@ class BilheteController{
         if(!(await schema.isValid(req.body))){
             return res.status(400).json({error: "Falha na Transmição de Dados"})
         }
-        const data = {id_sessao: req.body.id_sessao, id_sala: req.body.id_sala, id_poltrona: req.body.id_poltrona, reservado: false}
+        const {id_sessao, id_sala, id_poltrona} = req.body
+        const data = {id_sessao, id_sala, id_poltrona, reservado: false}
         await Bilhete.create(data);
 
         return res.status(200).json({sucesso: "Bilhete Criado"})
