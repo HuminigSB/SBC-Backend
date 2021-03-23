3790b30c898c6519addd9cdf951a043a99b6cf49
MatheusINFO <matheus.add12@gmail.com>
2021-03-23 14:29:21 -0300

    Adicionado crud para combo

--- src/app/models/Combo.js
+++ src/app/models/Combo.js
@@ -4,6 +4,7 @@ class Combo extends Model{
     static init(sequelize){
         super.init({
             id_user: Sequelize.INTEGER,
+            value: Sequelize.STRING,
             type: Sequelize.ENUM('none', 'popcorn', 'drink', 'popcorn + drink')
         },{
             sequelize
